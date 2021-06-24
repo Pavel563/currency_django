92bf913c1b33ce9d03491b189596bd54b2a843d1
Pavel563 <pasha.shalimoff169@gmail.com>
2021-06-24 19:57:43 +0300

    Add flake8, Travis CI, make model ContactUs

--- app/currency/models.py
+++ app/currency/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Rate(models.Model):
+    type = models.CharField(max_length=5)
+    sale = models.DecimalField(max_digits=5, decimal_places=2)
+    buy = models.DecimalField(max_digits=5, decimal_places=2)
+    created = models.DateTimeField(auto_now_add=True)
+    source = models.CharField(max_length=64)
+
+
+class ContactUs(models.Model):
+    id = models.IntegerField
+    email_from = models.CharField(max_length=50)
+    subject = models.CharField(max_length=50)
+    message = models.CharField(max_length=100)
--- app/currency/views.py
+++ app/currency/views.py
@@ -1,5 +1,12 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 from django.http import HttpResponse
+from currency.utils import generate_password as gp
+
 
 def hello(reqiuests):
     return HttpResponse("Hello World!")
+
+
+def generate_password(reqiuests):
+    password = gp()
+    return HttpResponse(password)
